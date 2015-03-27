20f50473f590a0e1ab4c0775082d0b0408a5e476
culmou1 <daehlinadeauotis@gmail.com>
2015-03-27 14:52:47 -0400

    ver.1
    Bon sa va être bon

---
+++ tp3/Header.h
@@ -0,0 +1,34 @@
+//
+//  Header.h
+//  tp3
+//
+//  Created by Daehli Nadeau Otis on 2015-03-25.
+//  Copyright (c) 2015 Daehli Nadeau-Otis. All rights reserved.
+//
+
+#ifndef tp3_Header_h
+#define tp3_Header_h
+using namespace std;
+
+struct noeud { // Je croix qu'on pourrait mettre la strucure dans le private de la classe
+    int valeur;
+    noeud *gauche;
+    noeud *droit;
+};
+class ABR {
+private:
+    noeud *racine; // La racine l’arbre
+public:
+    ABR ();// Construit l’arbre dont la racine est à l’adresse racine. 1
+    ~ABR(); // Désalloue l’espace mémoire occupé par l’arbre. // Le destructeur ne peut pas avoir des //////parametre
+    void Inserer(noeud *racine, int d); // insère le nœud de valeur d à l’arbre.
+    void Supprimer(noeud *racine, int d); // Supprime le nœud de valeur d de l’arbre.
+    void Afficher_Arbre(noeud *racine); /*Affiche les éléments de l’arbre niveau par niveau. Un bonus de
+    5pts est donné à ceux qui commencent par le premier niveau, ensuite le deuxième niveau, ainsi de suite, jusqu’à arriver à la racine ; le premier niveau étant l’ensemble des nœuds qui n’ont pas de descendants.*/
+    int Afficher_hauteur(noeud *racine); //Affiche la hauteur de l’arbre.
+    void Afficher_Ascendant(noeud *racine, int d); // Affiche les ascendants du nœud de valeur d.
+    void Archiver (noeud *racine);// Archiver en implémentation séquentielle l’arbre dont la racine est racine
+    //dans un fichier texte que vous allez définir. (bonus 10pts)
+}; // fin de la déclaration de la classe ABR
+
+#endif
--- tp3/main.cpp
+++ tp3/main.cpp
@@ -6,10 +6,128 @@
 //  Copyright (c) 2015 Daehli Nadeau-Otis. All rights reserved.
 //
 
+/*----------------Bibliothèque----------------*/
 #include <iostream>
+#include <fstream>
+#include "Header.h"
+using namespace std;
+
+/*----------------Function----------------*/
+string lireFichier(string lecture);
+void enrFichier(string);
+
+/*----------------Classe----------------*/
+ABR :: ABR(){
+    racine = NULL;
+}
+
+ABR ::~ABR(){
+    racine = NULL;
+}
+
+void ABR ::Inserer(noeud *racine, int d){
+    noeud *feuille = new noeud;
+    feuille->valeur=d;
+    if (racine == NULL){
+        racine = feuille;
+        return;
+    };
+    
+    noeud *courant = new noeud; // créer une nouvelle structure
+    courant = racine;
+    noeud *parent = new noeud; // créer une nouvelle structure
+    while(courant != NULL)
+    {
+        parent = courant;
+        if(d <= courant->valeur)
+        {
+            courant = courant->gauche;
+            
+        }else
+        {
+            courant= courant->droit;
+            
+        }
+    }
+    
+    //courant = feuille;
+    if(feuille->valeur <= parent->valeur)
+        parent->gauche=feuille;
+    else
+        parent->droit=feuille;
+    
+}
+void ABR::Supprimer(noeud *racine, int d){
+    if (racine == NULL)
+    {
+        cout<<" Il n'y a aucune donner dans cette arbre"<<endl;
+        return;
+    }
+    noeud *courant = racine;
+    noeud *parent;
+    while (current != NULL)
+    {
+        if (current->getData() == data)
+        {
+            return true;
+        }
+        
+        if(data < current->getData())
+        {
+            current = current->getLeftChild();
+        }else{
+            current = current->getRightChild();
+        }
+    }
+    
+    return false;
+}
+    
+
+}
+void ABR:: Afficher_Arbre(noeud *racine){
+    
+
+}
+int ABR ::Afficher_hauteur(noeud *racine){
+}
+void ABR:: Afficher_Ascendant(noeud *racine, int d){
+}
+void ABR:: Archiver (noeud *racine){
+}
+
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
+}
+/* -------------------------------------------------------------------------------*/
+/* DESCRIPTION :      Fonction principale du programmme								*/
+/* PARAMETRES :		  Aucun															*/
+/* VALEUR DE RETOUR : Aucune 														*/
+/* REMARQUE : 		  Aucune														*/
+/* -------------------------------------------------------------------------------*/
+
+string lireFichier(string lecture ){
+    ifstream lire(lecture.c_str(), ios::in);
+    if  (lire.fail()){
+        cout << "Erreur pour l'ouverture du fichier" <<endl;
+    }
+    else{
+        while (!lire.eof()) {
+            string commande;
+            getline(lire,commande);
+            cout << commande; //Pour voir l'intérieur tu contenu.
+            
+            
+        }
+    }
+    
+    
+    
+};
+
+void enrFichier(string){
 }
