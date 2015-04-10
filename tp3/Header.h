1b7bc635055538e62295a8cdd83ba193b4d874ab
gchaville <gillian.chaville@gmail.com>
2015-04-10 18:32:57 -0400

    Update Header.h

--- tp3/Header.h
+++ tp3/Header.h
@@ -15,23 +15,24 @@ struct noeud { // Je croix qu'on pourrait mettre la strucure dans le private de 
     noeud *gauche;
     noeud *droit;
 };
+
 class ABR {
 private:
     noeud *racine; // La racine l’arbre
 public:
     ABR ();// Construit l’arbre dont la racine est à l’adresse racine. 1
-    ~ABR(); /* Désalloue l’espace mémoire occupé par l’arbre. // Le destructeur ne peut pas avoir des parametre*/
-    noeud* getRacine();
-    void Inserer(noeud *&racine, int d); // insère le nœud de valeur d à l’arbre.
-    void Supprimer(noeud *racine, int d);// Supprime le nœud de valeur d de l’arbre.
-    noeud* supprimerMin(noeud *&racine);
+    ~ABR(); // Désalloue l’espace mémoire occupé par l’arbre. // Le destructeur ne peut pas avoir des //////parametre
+	noeud* getRacine (); //retourne la racine de l'arbre
+    void Inserer(noeud *&racine, int d); // insère le noeud de valeur d à l’arbre.
+    void Supprimer(noeud *&racine, int d); // Supprime le nœud de valeur d de l’arbre.
+	noeud* SupprimerMin(noeud *&racine);
     void Afficher_Arbre(noeud *racine, int niveau); /*Affiche les éléments de l’arbre niveau par niveau. Un bonus de
-    5pts est donné à ceux qui commencent par le premier niveau, ensuite le deuxième niveau, ainsi de suite, jusqu’à arriver à la racine ; le premier niveau étant l’ensemble des nœuds qui n’ont pas de descendants.*/
-    int Afficher_hauteur(noeud *&racine); //Affiche la hauteur de l’arbre.
-    noeud Afficher_Ascendant(noeud *racine, int d); // Affiche les ascendants du nœud de valeur d.
-    void Afficher_descendant(noeud *racine, int d);
-    void Archiver (noeud *racine); /*Archiver en implémentation séquentielle l’arbre dont la racine est racine
-    dans un fichier texte que vous allez définir. (bonus 10pts)
-fin de la déclaration de la classe ABR*/
-};
+    5pts est donné à ceux qui commencent par le premier niveau, ensuite le deuxième niveau, ainsi de suite, jusqu’à arriver à la racine ; le premier niveau étant l’ensemble des noeuds qui n’ont pas de descendants.*/
+    int Afficher_hauteur(noeud *racine); //Affiche la hauteur de l’arbre.
+    void Afficher_Ascendant(noeud *racine, int d); // Affiche les ascendants du noeud de valeur d.
+	void Instruction(noeud*&racine, char lecture,int valeur);
+    void Archiver (noeud *racine);// Archiver en implémentation séquentielle l’arbre dont la racine est racine
+    //dans un fichier texte que vous allez définir. (bonus 10pts)
+}; // fin de la déclaration de la classe ABR
+
 #endif
